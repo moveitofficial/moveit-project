aa542faf1196d72326ddf7d1e3c6eb53858141d4
apple <apple@apples-MacBook-Air.local>
2026-05-21 16:22:47 +0900

    feat: modal component

---
+++ packages/ui/src/Modal/index.ts
@@ -0,0 +1,9 @@
+export { default as Modal, ModalBody, ModalFooter, ModalHeader } from './Modal';
+export type { ModalProps, ModalSectionProps } from './Modal';
+
+export { default as ConfirmModal } from './ConfirmModal';
+export type {
+  ConfirmModalAction,
+  ConfirmModalActionVariant,
+  ConfirmModalProps,
+} from './ConfirmModal';
