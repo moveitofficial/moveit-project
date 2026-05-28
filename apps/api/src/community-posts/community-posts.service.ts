f339c49edbc6795f6b89a4c8e1a9399b8ab5f5ca
JoohyoungJun <bmwjjh1130@gmail.com>
2026-05-28 22:11:00 +0900

    [feat]: 커뮤니티 포스트 request dto 작성 및 서비스 로직 추가

--- apps/api/src/community-posts/community-posts.service.ts
+++ apps/api/src/community-posts/community-posts.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CommunityPost } from '@prisma/client';
+
+import { CommunityPostsRepository } from './community-posts.repository';
+import { PostRequestDto } from './dto/post-request.dto';
 
 @Injectable()
-export class CommunityPostsService {}
+export class CommunityPostsService {
+  constructor(
+    private readonly communityPostsRepository: CommunityPostsRepository,
+  ) {}
+
+  createPost(userId: string, dto: PostRequestDto): Promise<CommunityPost> {
+    return this.communityPostsRepository.createPost(userId, dto);
+  }
+}
---
+++ apps/api/src/community-posts/dto/post-request.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { CommunityCategory } from '@prisma/client';
+import { IsEnum, IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class PostRequestDto {
+  @ApiProperty({ example: CommunityCategory.QUESTION })
+  @IsEnum(CommunityCategory)
+  @IsNotEmpty()
+  declare category: CommunityCategory;
+
+  @ApiProperty({
+    example: '이 문제 어떻게 해결하는지 아시는 분?',
+    description: '게시글 제목',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 100)
+  declare title: string;
+
+  @ApiProperty({
+    example: '안녕하세요. 질문 드리겠습니다. ...',
+    description: '게시글 내용',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 1000)
+  declare content: string;
+}
