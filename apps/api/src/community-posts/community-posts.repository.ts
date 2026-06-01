f6e5d4859fe84c9db1724b10c031631d3e21734f
JoohyoungJun <bmwjjh1130@gmail.com>
2026-06-02 02:47:20 +0900

    [feat]: 게시글 repository findAllComments(), buildCommentListWhere() 추가

--- apps/api/src/community-posts/community-posts.repository.ts
+++ apps/api/src/community-posts/community-posts.repository.ts
@@ -4,6 +4,7 @@ import { Comment, CommunityCategory, CommunityPost } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 
 import {
+  commentListSelect,
   PostDetailItem,
   postDetailSelect,
   postListSelect,
@@ -144,5 +145,22 @@ export class CommunityPostsRepository {
     };
 
     return this.prisma.comment.create(args);
+  }
+
+  findAllComments(args: { postId: string; skip: number; take: number }) {
+    return this.prisma.comment.findMany({
+      where: this.buildCommentListWhere(args.postId),
+      select: commentListSelect,
+      orderBy: { createdAt: 'desc' },
+      skip: args.skip,
+      take: args.take,
+    });
+  }
+
+  buildCommentListWhere(postId: string) {
+    return {
+      postId,
+      deletedAt: null,
+    };
   }
 }
