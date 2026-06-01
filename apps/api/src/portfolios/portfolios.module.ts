9991f8b5734fe0412946bdd0c2c5657faacd681d
Eungyeol Baek <eungyeolBaek@gmail.com>
2026-06-02 01:30:24 +0900

    [fix]: 내 포트폴리오 목록 조회 라우트 충돌 수정 및 Swagger 태그 순서 정리

--- apps/api/src/portfolios/portfolios.module.ts
+++ apps/api/src/portfolios/portfolios.module.ts
@@ -3,14 +3,13 @@ import { Module } from '@nestjs/common';
 import { ExpertProfilesModule } from '../expert-profiles/expert-profiles.module';
 import { UploadModule } from '../upload/upload.module';
 
-import { MePortfoliosController } from './me-portfolios.controller';
 import { PortfoliosController } from './portfolios.controller';
 import { PortfoliosRepository } from './portfolios.repository';
 import { PortfoliosService } from './portfolios.service';
 
 @Module({
   imports: [ExpertProfilesModule, UploadModule],
-  controllers: [PortfoliosController, MePortfoliosController],
+  controllers: [PortfoliosController],
   providers: [PortfoliosService, PortfoliosRepository],
   exports: [PortfoliosService],
 })
