6891d274fe2ebda64073653ff552477554690099
hyojin-10 <wowshally@gmail.com>
2026-06-07 22:03:46 +0900

    chore: 스크롤 영역 수정

--- apps/admin/src/components/common/ListLayout/ListLayout.css.ts
+++ apps/admin/src/components/common/ListLayout/ListLayout.css.ts
@@ -3,6 +3,8 @@ import { style } from '@vanilla-extract/css';
 export const container = style({
   display: 'flex',
   flexDirection: 'column',
+  flex: 1,
+  minHeight: 0,
 });
 
 export const filterSection = style({
@@ -14,6 +16,9 @@ export const filterSection = style({
 
 export const tableSection = style({
   marginTop: '24px',
+  flex: '1 1 0',
+  minHeight: 0,
+  overflowY: 'auto',
 });
 
 export const pagination = style({
