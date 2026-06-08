31089f46af0554aea3977f81a69d1c32d360245b
JoohyoungJun <bmwjjh1130@gmail.com>
2026-06-08 14:26:56 +0900

    [feat]: 게시글 module NotificaitonModule import

--- apps/api/src/community-posts/community-posts.module.ts
+++ apps/api/src/community-posts/community-posts.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { NotificationsModule } from '../notifications/notifications.module';
+
 import { CommunityPostsController } from './community-posts.controller';
 import { CommunityPostsRepository } from './community-posts.repository';
 import { CommunityPostsService } from './community-posts.service';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [CommunityPostsController],
   providers: [CommunityPostsService, CommunityPostsRepository],
 })
