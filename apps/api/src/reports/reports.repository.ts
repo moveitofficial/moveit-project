575061e52fb3cce410dc92f23d74777f11e046d8
JoohyoungJun <bmwjjh1130@gmail.com>
2026-06-11 17:46:44 +0900

    [feat]: reports repository create() 추가

--- apps/api/src/reports/reports.repository.ts
+++ apps/api/src/reports/reports.repository.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Report } from '@prisma/client';
 
 import { PrismaService } from '../prisma/prisma.service';
+
+import { ReportsRequestDto } from './dto/reports-request.dto';
 
 @Injectable()
 export class ReportsRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  create(reporterId: string, dto: ReportsRequestDto): Promise<Report> {
+    return this.prisma.report.create({
+      data: {
+        reporterId,
+        reportedId: dto.reportedUserId,
+        reason: dto.reason,
+        detail: dto.detail,
+        images: {
+          create: dto.imageUrls?.map((imageUrl) => ({ imageUrl })) ?? [],
+        },
+      },
+    });
+  }
 }
