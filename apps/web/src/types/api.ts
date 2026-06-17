a5358fe0a4985e8382920f720232db0be5187e09
hyojin-10 <wowshally@gmail.com>
2026-06-18 01:55:47 +0900

    feat: 공용 유틸 작성(param 관련, scroll 관련)

--- apps/web/src/types/api.ts
+++ apps/web/src/types/api.ts
@@ -3,3 +3,20 @@ export interface ApiSuccess<T> {
   message: string;
   data: T;
 }
+
+export interface Pagination {
+  page: number;
+  pageSize: number;
+  totalCount: number;
+  hasNext: boolean;
+}
+
+export interface PaginatedResult<T> {
+  items: T[];
+  pagination: Pagination;
+}
+
+export interface InfiniteScrollPage<T> {
+  items: T[];
+  hasNext: boolean;
+}
