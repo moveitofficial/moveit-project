2393f46936a7e1ca55581e2a9146c5f4cdd62f24
BackNo23 <kuzhi.no.23@gmail.com>
2026-06-19 10:49:05 +0900

    이름없음 수정

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -19,7 +19,9 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'phinf.pstatic.net' },
       { protocol: 'https', hostname: 'k.kakaocdn.net' },
+      { protocol: 'http', hostname: 'k.kakaocdn.net' },
       { protocol: 'https', hostname: 'img1.kakaocdn.net' },
+      { protocol: 'http', hostname: 'img1.kakaocdn.net' },
     ],
   },
 };
